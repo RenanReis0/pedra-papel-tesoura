3bb9778b481781e7b4569346ad764f5517288b33
RenanReis0 <renan06santos05@gmail.com>
2025-04-10 02:02:35 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,70 @@
+let humanScore = 0;
+let computerScore = 0;
+let empates = 0
+
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random()*3)
+    switch (randomNumber){
+        case 0:
+            return "pedra"
+        case 1:
+            return "papel"
+        case 2:
+            return "tesoura"
+    }
+}
+
+function getHumanChoice() {
+    let escolha = prompt("Escolha pedra, papel ou tesoura").toLowerCase()
+    if(escolha == "pedra"){
+        return escolha
+    } else if (escolha == "papel"){
+        return escolha
+    } else if (escolha == "tesoura"){
+        return escolha
+    } else{
+        return getHumanChoice()
+    }
+}
+
+function playRound(computerChoice, humanChoice) {
+    if (
+        (computerChoice === "pedra" && humanChoice === "papel")||
+        (computerChoice === "papel" && humanChoice === "tesoura")||
+        (computerChoice === "tesoura" && humanChoice === "pedra")
+    ){
+        humanScore++;
+        alert(`${humanChoice} vence ${computerChoice}. jogador ganhou!`);
+    } else if (
+        (computerChoice === "pedra" && humanChoice === "tesoura")||
+        (computerChoice === "papel" && humanChoice === "pedra")||
+        (computerChoice === "tesoura" && humanChoice === "papel")
+    ){
+        computerScore++
+        alert(`${computerChoice} vence ${humanChoice}. Computador ganhou!`)
+    } else if(computerChoice == humanChoice){
+        alert(`Deu empate!`)
+        empates++
+    }
+    
+}
+
+
+
+
+function playGame(){
+    for(let p = 1; p<=5; p++){
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        playRound(computerChoice, humanChoice);
+        
+    }
+    if(humanScore>computerScore){
+        alert(`Pontuação jogador: ${humanScore}.`+"\n"+`Pontuação computador: ${computerScore}.`+"\n"+`Empates: ${empates}`+"\n"+`Jogador ganhou!`);
+    } else{
+        alert(`Pontuação jogador: ${humanScore}.`+"\n"+`Pontuação computador: ${computerScore}.`+"\n"+`Empates: ${empates}`+"\n"+`Computador ganhou!`);
+    }
+    
+}
+playGame()
